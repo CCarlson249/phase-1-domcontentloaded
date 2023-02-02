7e52d56a5dbb76df96ba35b8b8bae050e76c4425
Clayton Carlson <claytonrc97@gmail.com>
2023-02-01 18:19:32 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+const tagChange = document.querySelector('p');
+tagChange.textContent = 'This is really cool!'
+});
